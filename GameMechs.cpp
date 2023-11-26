95a3376eef57216f499835c0b8143d3e869e4c0b
hannaj19 <hannaj19@mcmaster.ca>
2023-11-26 17:32:31 -0500

    basic game mechs implementation

--- GameMechs.cpp
+++ GameMechs.cpp
@@ -2,52 +2,52 @@
 
 GameMechs::GameMechs()
 {
-
+    input = 0;
+    exitFlag = false;
+    boardSizeX = 10;
+    boardSizeY = 10;
 }
 
 GameMechs::GameMechs(int boardX, int boardY)
 {
-
+    input = 0;
+    exitFlag = false;
+    boardSizeX = boardX;
+    boardSizeY = boardY;
 }
 
 // do you need a destructor?
 
-
-
 bool GameMechs::getExitFlagStatus()
 {
-
+    return exitFlag;
 }
 
 char GameMechs::getInput()
 {
-
 }
 
 int GameMechs::getBoardSizeX()
 {
-
+    return boardSizeX;
 }
 
 int GameMechs::getBoardSizeY()
 {
-
+    return boardSizeY;
 }
-
 
 void GameMechs::setExitTrue()
 {
-
+    exitFlag = true;
 }
 
 void GameMechs::setInput(char this_input)
 {
-
+    input = this_input;
 }
 
 void GameMechs::clearInput()
 {
-
+    input = 0;
 }
-
-
