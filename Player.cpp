4a977333471d4ed6f278b20fcf47d159167ca8c7
hannaj19 <hannaj19@mcmaster.ca>
2023-11-26 20:15:12 -0500

    implement most of player

--- Player.cpp
+++ Player.cpp
@@ -1,32 +1,66 @@
 #include "Player.h"
+#include "objPosArrayList.h"
 
-
-Player::Player(GameMechs* thisGMRef)
+Player::Player(GameMechs *thisGMRef)
 {
     mainGameMechsRef = thisGMRef;
     myDir = STOP;
-
+    playerPos = new objPosArrayList();
+    playerPos->insertHead({1, 1, '@'});
     // more actions to be included
 }
-
 
 Player::~Player()
 {
     // delete any heap members here
+    delete[] playerPos;
 }
 
 void Player::getPlayerPos(objPos &returnPos)
 {
-    // return the reference to the playerPos arrray list
+    // return the reference to the playerPos array list
+    playerPos->getHeadElement(returnPos);
 }
 
 void Player::updatePlayerDir()
 {
-    // PPA3 input processing logic        
+    // PPA3 input processing logic
 }
 
 void Player::movePlayer()
 {
     // PPA3 Finite State Machine logic
+    switch (myDir)
+    {
+    case STOP:
+        break;
+    case UP:
+        objPos currentPos;
+        playerPos->getHeadElement(currentPos);
+        currentPos.y -= 1;
+        playerPos->insertHead(currentPos);
+        playerPos->removeTail();
+        break;
+    case DOWN:
+        objPos currentPos;
+        playerPos->getHeadElement(currentPos);
+        currentPos.y += 1;
+        playerPos->insertHead(currentPos);
+        playerPos->removeTail();
+        break;
+    case LEFT:
+        objPos currentPos;
+        playerPos->getHeadElement(currentPos);
+        currentPos.x -= 1;
+        playerPos->insertHead(currentPos);
+        playerPos->removeTail();
+        break;
+    case RIGHT:
+        objPos currentPos;
+        playerPos->getHeadElement(currentPos);
+        currentPos.x += 1;
+        playerPos->insertHead(currentPos);
+        playerPos->removeTail();
+        break;
+    }
 }
-
