19ec495df8296940c39908ccd794b729ebf71f75
langlt2 <langlt2@mcmaster.ca>
2023-11-26 23:56:36 -0500

    fixed things

---
+++ Definitions.h
@@ -0,0 +1,10 @@
+#ifndef DEFINITIONS_H
+#define DEFINITIONS_H
+
+#define DELAY_CONST 100000
+#define FOOD_LENGTH 1
+#define WIN_LENGTH 4
+#define BOARD_WIDTH 20
+#define BOARD_HEIGHT 10
+# define FOOD_CHAR 36
+#endif
--- GameMechs.cpp
+++ GameMechs.cpp
@@ -1,5 +1,7 @@
 #include "GameMechs.h"
-
+#include "Definitions.h"
+#include "Player.h"
+#include "objPosArrayList.h"
 GameMechs::GameMechs()
 {
     input = 0;
@@ -55,4 +57,46 @@ void GameMechs::clearInput()
 
 objPosArrayList GameMechs::generateFood(Player *player)
 {
+    int RandomNum;
+    int CheckArray[BOARD_WIDTH][BOARD_HEIGHT];
+    for (int j = 0; j < getBoardSizeY(); j++)
+    {
+        for (int i = 0; i < getBoardSizeX(); i++)
+        {
+            if (j == 0 || j == Board1.getBoardSizeY() - 1 ||
+                i == 0 || i == Board1.getBoardSizeX() - 1)
+            {
+                CheckArray[i][j] = 1;
+            }
+        }
+    }
+
+    objPosArrayList PlayerPositions;
+    player->getPlayerPos(PlayerPositions);
+    PlayerPositions.getSize();
+
+    for (int w = 0; w < PlayerPositions.getSize(); w++)
+    {
+        objPos playerPos;
+        PlayerPositions.getElement(playerPos, w);
+        CheckArray[PlayerPos.x][PlayerPos.y] = 1;
+    }
+
+    objPosArrayList FoodPositions;
+}
+
+void Random(objPosArrayList ReturnArray, int CheckArray[][BOARD_HEIGHT], int min, int max)
+{
+    int num1 = (rand() % (max - min - 1)) + min;
+    int num2 = (rand() % (max - min - 1)) + min;
+
+    if (CheckArray[num1][num2] == 1)
+    {
+        Random(ReturnArray, CheckArray, min, max);
+    }
+    else
+    {
+        CheckArray[num1][num2]++;
+        ReturnArray.insertHead({num1, num2, FOOD_CHAR});
+    }
 }
