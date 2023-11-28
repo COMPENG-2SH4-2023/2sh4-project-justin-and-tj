6acbfe54d725cd03164987752ca057a187776e3a
hannaj19 <hannaj19@mcmaster.ca>
2023-11-27 21:28:39 -0500

    add player documentation

--- Player.cpp
+++ Player.cpp
@@ -1,6 +1,11 @@
 #include "Player.h"
 #include "objPosArrayList.h"
 
+/**
+ * @brief Construct a new Player object
+ *
+ * @param thisGMRef Game Mechanics reference for game board interactions
+ */
 Player::Player(GameMechs *thisGMRef)
 {
     mainGameMechsRef = thisGMRef;
@@ -17,11 +22,18 @@ Player::~Player()
     delete playerPos;
 }
 
+/**
+ * @brief Get the ArrayList containing the player's position data
+ *
+ * @param returnPos ArrayList containing a copy of the current player position objects
+ */
 void Player::getPlayerPos(objPosArrayList &returnPos)
 {
+    // Empty incoming Array List
     for (int i = 0; i < returnPos.getSize(); i++)
         returnPos.removeHead();
 
+    // Populate with position data from this Array List
     for (int i = 0; i < playerPos->getSize(); i++)
     {
         objPos currentEl;
@@ -30,14 +42,27 @@ void Player::getPlayerPos(objPosArrayList &returnPos)
     }
 }
 
+/**
+ * @brief Get the position of the player's head element
+ *
+ * @param returnPos Position that will contain head element
+ */
 void Player::getPlayerHeadPos(objPos &returnPos)
 {
-    // return the reference to the head of the playerPos array list
     playerPos->getHeadElement(returnPos);
 }
 
+/**
+ * @brief Check if coordinate is occupied by this player
+ *
+ * @param x X-Coordinate to check
+ * @param y Y-Coordiante to check
+ * @return true if coordinate is occupied by player
+ * @return false if coordinate is not occupied by player
+ */
 bool Player::isPlayerPos(int x, int y)
 {
+
     for (int i = 0; i < playerPos->getSize(); i++)
     {
         objPos currentPosition;
@@ -50,11 +75,20 @@ bool Player::isPlayerPos(int x, int y)
     return 0;
 }
 
+/**
+ * @brief Get a player's direction
+ *
+ * @return int Current Direction based on the Dir Enumeration
+ */
 int Player::getDirection()
 {
     return myDir;
 }
 
+/**
+ * @brief Update the current player's direction based on user input
+ *
+ */
 void Player::updatePlayerDir()
 {
     // PPA3 input processing logic
@@ -75,20 +109,27 @@ void Player::updatePlayerDir()
     {
         myDir = DOWN;
     }
+    // Escape case to close game before completion
     else if (cmd == 32)
     {
         mainGameMechsRef->setExitTrue();
     }
 }
 
+/**
+ * @brief Move the player one step based on the current direction
+ *
+ */
 void Player::movePlayer()
 {
+    // Get the current head and food positions
     objPos currentPos;
-    getPlayerHeadPos(currentPos);
     objPos foodPos;
+    getPlayerHeadPos(currentPos);
     mainGameMechsRef->getFoodPos(foodPos);
 
     // PPA3 Finite State Machine logic
+    // Get game board dimensions
     int boardX = mainGameMechsRef->getBoardSizeX();
     int boardY = mainGameMechsRef->getBoardSizeY();
 
@@ -97,11 +138,13 @@ void Player::movePlayer()
     case STOP:
         break;
     case UP:
-
+        // Wrap-Around logic case
         if (currentPos.y - 1 == 0)
         {
+            // Check for self-collision in wrap-around case
             if (playerPos->isElement({currentPos.x, boardY - 2, '@'}))
             {
+                // Mark game as complete and finish loop
                 mainGameMechsRef->setWinTrue();
                 mainGameMechsRef->setExitTrue();
             }
@@ -112,8 +155,10 @@ void Player::movePlayer()
         }
         else
         {
+            // Check for self-collision in normal case
             if (playerPos->isElement({currentPos.x, currentPos.y - 1, '@'}))
             {
+                // Mark game as complete and finish loop
                 mainGameMechsRef->setWinTrue();
                 mainGameMechsRef->setExitTrue();
             }
@@ -123,12 +168,19 @@ void Player::movePlayer()
             }
         }
 
+        // Insert new position to the front of the playerPositions
         playerPos->insertHead(currentPos);
 
+        // Check for non-food case
         if (!playerPos->isElement(foodPos))
         {
+            // Remove last playerPosition to make the snake appear like it is moving
             playerPos->removeTail();
         }
+        // For the previous section, we want to "move" the snake if there's no food, and if there is food we leave the last element
+        // and have the snake grow longer by one.
+
+        // The structure is identical for all the other cases, just with varying wrap-around and movement positions
         break;
     case DOWN:
         if (currentPos.y + 1 == boardY - 1)
@@ -229,6 +281,7 @@ void Player::movePlayer()
         break;
     }
 
+    // Refresh food positions and award points if the player moved onto a food object
     if (playerPos->isElement(foodPos))
     {
         objPosArrayList playerPositions;
